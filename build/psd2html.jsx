d9dc2617b3776f8632a642bb0d71f7d3143a7915
YusukeHirao <u10.bass@i.softbank.jp>
2013-03-08 17:09:39 +0900

    dev v@1.0.0 r19 (grunt)

--- build/psd2html.jsx
+++ build/psd2html.jsx
@@ -728,7 +728,6 @@ if (documents.length) {
         activeDocument.save();
         input();
       } catch (err) {
-        alert(this.filePutDialog);
         saved = !!this.filePutDialog('ドキュメントの保存', 'document.psd', 'Egg psd');
       }
       $.level = _level;
