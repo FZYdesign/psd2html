1d06e42656abd8a1226a5928587a2dc915d1649c
YusukeHirao <u10.bass@i.softbank.jp>
2013-03-11 11:34:49 +0900

    dev v@1.1.0 r94 (grunt)

--- build/psd2html.jsx
+++ build/psd2html.jsx
@@ -1,5 +1,5 @@
 ﻿/**
- * psd2html.js - v@1.1.0 r93
+ * psd2html.js - v@1.1.0 r94
  * update: 2013-03-11
  * Author: Yusuke Hirao [http://www.yusukehirao.com]
  * Github: https://github.com/YusukeHirao/psd2html
