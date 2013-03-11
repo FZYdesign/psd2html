ca7a81f381e653943684312252cedeb58397dc1f
YusukeHirao <u10.bass@i.softbank.jp>
2013-03-11 17:29:47 +0900

    dev v@1.1.0 r152 (grunt)

--- build/psd2html.jsx
+++ build/psd2html.jsx
@@ -1,5 +1,5 @@
 ﻿/**
- * psd2html.js - v@1.1.0 r151
+ * psd2html.js - v@1.1.0 r152
  * update: 2013-03-11
  * Author: Yusuke Hirao [http://www.yusukehirao.com]
  * Github: https://github.com/YusukeHirao/psd2html
@@ -700,6 +700,9 @@ showLayer = function(layer) {
 
 extract = function(layer, mix, extFlag, originalText) {
   var data, dir, ext, fillTransparent, metrics, name, newDoc, url;
+  if (originalText == null) {
+    originalText = [];
+  }
   name = layer.name;
   if (ext = name.match(/(\.(?:jpe?g|gif|png))$/i)) {
     ext = ext[0];
